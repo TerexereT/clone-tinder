5e6d7301f96125ee0d55729dc52a06cb4bdc7826
Armando Rivas <armandojrivasc@gmail.com>
2022-01-12 00:08:08 -0400

    theme provider, because why not?

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
+const theme = createTheme({});
+
 ReactDOM.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>,
+	<StrictMode>
+		<ThemeProvider theme={theme}>
+			<App />
+		</ThemeProvider>
+	</StrictMode>,
 	document.getElementById('root')
 );
