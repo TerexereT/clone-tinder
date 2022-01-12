5c7fd4b2d50185324a002887b35d43af7ecda444
Armando Rivas <armandojrivasc@gmail.com>
2022-01-12 00:08:28 -0400

    full header component layout

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,43 @@
-import React from 'react';
-import './header.css';
+import ForumIcon from '@mui/icons-material/Forum';
+import PersonIcon from '@mui/icons-material/Person';
+import { IconButton, Theme } from '@mui/material';
+import { makeStyles } from '@mui/styles';
+import { FC } from 'react';
 
-const Header = () => {
+const useStyles = makeStyles((theme: Theme) => ({
+	header: {
+		display: 'flex',
+		alignItems: 'center',
+		justifyContent: 'space-between',
+		zIndex: 100,
+		borderBottom: '1px solid #f9f9f9',
+	},
+	icon: {
+		padding: 20,
+	},
+	logo: {
+		objectFit: 'contain',
+		height: 40,
+	},
+}));
+
+const Header: FC = () => {
+	const classes = useStyles();
 	return (
-		<div className='header'>
-			<h2>soy header</h2>
+		<div className={classes.header}>
+			<IconButton>
+				<PersonIcon fontSize='large' className={classes.icon} />
+			</IconButton>
+
+			<img
+				alt=''
+				src='https://1000logos.net/wp-content/uploads/2018/07/Tinder-icon.png'
+				className={classes.logo}
+			/>
+
+			<IconButton>
+				<ForumIcon fontSize='large' className={classes.icon} />
+			</IconButton>
 		</div>
 	);
 };
