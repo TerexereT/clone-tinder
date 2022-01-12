1842dd2f81def4da28117e17efdaf7880771f52c
Armando Rivas <armandojrivasc@gmail.com>
2022-01-11 23:22:41 -0400

    first commit with the set up

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { FC } from 'react';
+import './App.css';
+import Header from './components/Header';
+
+const App: FC = () => {
+	return (
+		<div className='App'>
+			{/* HEADER */}
+			<Header />
+			{/* Cartas */}
+
+			{/* Botones */}
+		</div>
+	);
+};
+
+export default App;
